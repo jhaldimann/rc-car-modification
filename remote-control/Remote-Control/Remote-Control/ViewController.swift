e0c26a7803926cfc2ecbe3fa0e17689b3c03177d
Julian Haldimann <julian@praxishaldimann.ch>
2021-08-08 10:05:32 +0200

    Add podfile and add some gitignore entries

---
+++ remote-control/Remote-Control/Remote-Control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remote-Control
+//
+//  Created by Julian Haldimann on 05.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
